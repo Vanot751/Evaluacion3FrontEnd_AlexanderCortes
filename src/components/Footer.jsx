77ae0a2fa13baeafb32bf91597223ba574407bfd
Vanot751 <167124531+Vanot751@users.noreply.github.com>
2026-07-01 19:41:40 -0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,7 +26,7 @@ export default function Footer() {
         </div>
         <hr className="bg-secondary" />
         <p className="text-center text-muted small">
-          &copy; {new Date().getFullYear()} FitnessPro - Todos los derechos reservados.
+          2026 FitnessPro - Todos los derechos reservados.
         </p>
       </div>
     </footer>
